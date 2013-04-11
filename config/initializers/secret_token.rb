ba7aea2dd5b87164dfb8dcf966c6f669cf833085
Christian Kyony <ckyony@changamuka.com>
2013-04-11 12:25:00 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrainGeo::Application.config.secret_token = 'ad031ce8966560dbec2b316864958a07c2bd5391f8d71378ee6f8a84d9e3ee6dc8f4fd26df76444112ee7844317638cf5ea9a06ccadd8131bf926914b8fa072e'
